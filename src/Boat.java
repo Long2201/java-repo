a8ee8a0e7b31466340522b2afe062a46b5346e5c
Long Vu <longvu@Longs-MBP.gateway>
2024-08-05 12:18:11 +0930

    Pushing code for checkout 18

--- src/Boat.java
+++ src/Boat.java
@@ -1,9 +1,11 @@
 public class Boat {
+    // Private instances of the class
     private String name = "unknown";
     private String boatClass = "unknown";
     private int regNum = -1;
     private double yardstick;
-
+    
+    // Class Constructors
     public Boat(){
         yardstick = 100.0;
     }
@@ -13,7 +15,37 @@ public class Boat {
         this.regNum = initRegNum;
         this.yardstick = initYardstick;
     }
+
+    //Functions to get instances of the class
+    public String getName(){
+        return this.name;
+    }
+    public String getBoatClass(){
+        return this.boatClass;
+    }
+    public int getNum(){
+        return this.regNum;
+    }
+    public double getYard(){
+        return this.yardstick;
+    }
+
+    //Functions to set instances of the class
+    public void setName(String initName){
+        this.name = initName;
+    }
+    public void setClass(String initBoatClass){
+        this.boatClass = initBoatClass;
+    }
+    public void setNum (int initRegNum){
+        this.regNum = initRegNum;
+    }
+    public void setYard(double initYardstick){
+        this.yardstick = initYardstick;
+    }
+
+    //Function to create the correct format of the string
     public String toString(){
         return "Boat " + this.name + ", Class = " + this.boatClass + ", Registration # = KA " + this.regNum;
-    }
+    } 
 }
--- src/BoatMaker.java
+++ src/BoatMaker.java
@@ -5,6 +5,11 @@ public class BoatMaker {
         Boat myBoat1 = new Boat("Harmony Blue", "International 505", 6467, 95 );
         System.out.println(myBoat.toString());
         System.out.println(myBoat1.toString());
+
+        //Extention of checkout 18 requirements
+        myBoat.setName("Australia II");
+        System.out.println(myBoat.getName());
+        System.out.println(myBoat1.getName());
     }
 }
 
